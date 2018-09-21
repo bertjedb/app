527f2e0ed4996913d898d155b969291f2ad9524e
TheElmo <jelmer.internet@xs4all.nl>
2018-09-21 16:13:26 +0200

    registration page

--- app/screens/Registration.js
+++ app/screens/Registration.js
@@ -11,7 +11,6 @@ import {
 } from 'react-native';
 import { DrawerActions } from 'react-navigation';
 import styles from '../assets/css/style.js';
-import {BoxShadow} from 'react-native-shadow';
 
 import {
     COLOR,
@@ -52,18 +51,6 @@ export default class Registration extends Component {
   }
 
   render() {
-    const shadowOpt = {
-            width:160,
-            height:170,
-            color:"#000",
-            border:2,
-            radius:3,
-            opacity:0.2,
-            x:0,
-            y:3,
-            style:{marginVertical:5}
-        }
-
     return(
         <ThemeContext.Provider value={getTheme(uiTheme)}>
             <Toolbar
@@ -76,60 +63,58 @@ export default class Registration extends Component {
                  onRightElementPress={() => this.props.navigation.navigate('ScannerQR')}
             />
             <View style= {styles.RegistrateBackground}>
-                <BoxShadow setting={shadowOpt}>
-                    <View style={styles.inputContainer}>
-                        <Text style={styles.rgstTitle}>
-                            Registreren
-                        </Text>
-                        <TextInput  style= {styles.inputField}
-                                    fontStyle = {'italic'}
-                                    label="First name"
-                                    placeholder="   Voornaam"
-                                    value={ this.state.firstName }
-                                    onChangeText={ email => this.setState({firstName}) }>
-                        </TextInput>
-                        <TextInput  style= {styles.inputField}
-                                    fontStyle = {'italic'}
-                                    label="Last name"
-                                    placeholder="   Achternaam"
-                                    value={ this.state.lastName }
-                                    onChangeText={ email => this.setState({lastName}) }>
-                        </TextInput>
-                        <TextInput  style= {styles.inputField}
-                                    fontStyle = {'italic'}
-                                    label="E-mail address"
-                                    placeholder="   E-mailadres"
-                                    value={ this.state.email }
-                                    onChangeText={ email => this.setState({email}) }>
-                        </TextInput>
-                        <TextInput  style= {styles.inputField}
-                                    fontStyle = {'italic'}
-                                    label="Password"
-                                    value={ this.state.password }
-                                    placeholder="   Wachtwoord"
-                                    onChangeText={ password => this.setState({firstPassword}) }
-                                    secureTextEntry={true}>
-                        </TextInput>
-                        <TextInput  style= {styles.inputField}
-                                    fontStyle = {'italic'}
-                                    label="Password"
-                                    value={ this.state.password }
-                                    placeholder="   Herhaal wachtwoord"
-                                    onChangeText={ password => this.setState({firstPassword}) }
-                                    secureTextEntry={true}
-                                    onSubmitEditing= { () => {
-                                        this.registrate();
-                                    }}>
-                        </TextInput>
-                        <Button
-                            style={{container: styles.rgstBtn}}
-                            raised text="Doorgaan"
-                            onPress={() => {
-                                this.registrate();
-                            }}>
-                        </Button>
-                    </View>
-                </BoxShadow>
+                <View style={styles.inputContainer}>
+                    <Text style={styles.rgstTitle}>
+                        Registreren
+                    </Text>
+                    <TextInput  style= {styles.inputField}
+                                fontStyle = {'italic'}
+                                label="First name"
+                                placeholder="   Voornaam"
+                                value={ this.state.firstName }
+                                onChangeText={ email => this.setState({firstName}) }>
+                    </TextInput>
+                    <TextInput  style= {styles.inputField}
+                                fontStyle = {'italic'}
+                                label="Last name"
+                                placeholder="   Achternaam"
+                                value={ this.state.lastName }
+                                onChangeText={ email => this.setState({lastName}) }>
+                    </TextInput>
+                    <TextInput  style= {styles.inputField}
+                                fontStyle = {'italic'}
+                                label="E-mail address"
+                                placeholder="   E-mailadres"
+                                value={ this.state.email }
+                                onChangeText={ email => this.setState({email}) }>
+                    </TextInput>
+                    <TextInput  style= {styles.inputField}
+                                fontStyle = {'italic'}
+                                label="Password"
+                                value={ this.state.password }
+                                placeholder="   Wachtwoord"
+                                onChangeText={ password => this.setState({firstPassword}) }
+                                secureTextEntry={true}>
+                    </TextInput>
+                    <TextInput  style= {styles.inputField}
+                                fontStyle = {'italic'}
+                                label="Password"
+                                value={ this.state.password }
+                                placeholder="   Herhaal wachtwoord"
+                                onChangeText={ password => this.setState({firstPassword}) }
+                                secureTextEntry={true}
+                                onSubmitEditing= { () => {
+                                    this.registrate();
+                                }}>
+                    </TextInput>
+                    <Button
+                        style={{container: styles.rgstBtn}}
+                        raised text="Doorgaan"
+                        onPress={() => {
+                            this.registrate();
+                        }}>
+                    </Button>
+                </View>
             </View>
         </ThemeContext.Provider>
     );
