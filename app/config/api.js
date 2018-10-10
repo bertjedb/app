b885f753531e57b7fa52e8962640311ce168c8e0
Wouter Kurvers <WKurvers11@gmail.com>
2018-10-10 15:18:51 +0200

    temp

--- app/config/api.js
+++ app/config/api.js
@@ -4,7 +4,7 @@ import LocalStorage from './localStorage.js';
 export default class Api {
 
 	static instance = null;
-	url= "http://gaauwe.nl:5000/"
+	url= "145.37.164.183:5000/"
 	static getInstance() {
 		if(Api.instance == null) {
 			Api.instance = new Api();
