e9e76b947e52cdd5fd9ef24218e9e4ff0b7962bd
unknown <jelmer.haarman@xs4all.nl>
2018-11-09 15:33:11 +0100

    intro screen launched once

--- app/index.js
+++ app/index.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { MyAppNotLoggedIn, MyAppLoggedIn } from './config/router';
-import { View, StatusBar, StyleSheet, Dimensions, Image, SafeAreaView } from 'react-native';
+import { View, StatusBar, StyleSheet, Dimensions, Image, SafeAreaView, AsyncStorage } from 'react-native';
 import LocalStorage from './config/localStorage.js';
 import { COLOR, ThemeContext, getTheme } from 'react-native-material-ui';
 import LinearGradient from 'react-native-linear-gradient';
@@ -8,6 +8,9 @@ import { Header } from 'react-navigation';
 import OneSignal from 'react-native-onesignal';
 import {PacmanIndicator} from 'react-native-indicators';
 import FlashMessage from "react-native-flash-message";
+import Intro from './screens/Intro.js';
+import Onboarding from 'react-native-onboarding-swiper';
+import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
 
 // you can set your style right here, it'll be propagated to application
 const uiTheme = {
@@ -26,35 +29,20 @@ class App extends Component {
     super();
     this.state = {
       userId: null,
-      loading: true
+      loading: true,
+      firstLaunch: false
     };
     console.disableYellowBox = true;
   }
 
   componentWillMount() {
     OneSignal.init("893db161-0c60-438b-af84-8520b89c6d93");
-
-    OneSignal.addEventListener("received", this.onReceived);
-    OneSignal.addEventListener("opened", this.onOpened);
     OneSignal.addEventListener("ids", this.onIds);
   }
 
   componentWillUnmount() {
-    OneSignal.removeEventListener("received", this.onReceived);
-    OneSignal.removeEventListener("opened", this.onOpened);
     OneSignal.removeEventListener("ids", this.onIds);
     clearTimeout(this.timeoutHandle);
-  }
-
-  onReceived(notification) {
-    console.log("Notification received: ", notification);
-  }
-
-  onOpened(openResult) {
-    console.log("Message: ", openResult.notification.payload.body);
-    console.log("Data: ", openResult.notification.payload.additionalData);
-    console.log("isActive: ", openResult.notification.isAppInFocus);
-    console.log("openResult: ", openResult);
   }
 
   onIds(device) {
@@ -68,6 +56,7 @@ class App extends Component {
     localStorage
       .retrieveItem("userId")
       .then(id => {
+        console.log(id)
         if (id != null) {
           this.setState({
             loggedIn: true
@@ -103,6 +92,13 @@ class App extends Component {
         //this callback is executed when your Promise is rejected
         console.log("Promise is rejected with error: OH BOII" + error);
       });
+      localStorage.retrieveItem("alreadyLaunched").then(value => {
+        if(value == null) {
+            //first time launched
+            this.setState({firstLaunch: true})
+            localStorage.storeItem("alreadyLaunched", true)
+        }
+      })
   }
 
   render() {
@@ -113,11 +109,46 @@ class App extends Component {
                     barStyle="light-content"
                 />
                 { this.update() }
-                {!this.state.loading &&
+                {!this.state.loading && (
                     <ThemeContext.Provider value={getTheme(uiTheme)}>
-                        {this.state.loggedIn ? <MyAppLoggedIn/> : <MyAppNotLoggedIn/>}
+                        {!this.state.loggedIn && !this.state.firstLaunch && <MyAppNotLoggedIn />}
+                        {this.state.loggedIn && this.state.clearance == 1 && !this.state.firstLaunch && <MyAppLoggedInAdmin />}
+                        {this.state.loggedIn && this.state.clearance == 0 && !this.state.firstLaunch && <MyAppLoggedInUser />}
+                        {this.state.firstLaunch && (
+                            <Onboarding
+                                onDone={() => {AsyncStorage.setItem('alreadyLaunched', true); this.setState({firstLaunch: false})}}
+                                onSkip={() => {AsyncStorage.setItem('alreadyLaunched', true); this.setState({firstLaunch: false})}}
+                                transitionAnimationDuration={250}
+                                pages={[
+                                {
+                                   backgroundColor: '#8bc34a',
+                                   image: <Image style={{height: '100%', width: '75%'}} resizeMode='contain' source={require('./assets/logo.png')} />,
+                                   title: 'Bslim',
+                                   subtitle: 'De meest geweldige app voor de sportende jeugd.',
+                                },
+                                {
+                                   backgroundColor: '#8bc34a',
+                                   image: <Icon size={120} name={ 'calendar-range' } style={{ color: 'white' }} />,
+                                   title: 'Evenementen',
+                                   subtitle: 'Op de evenementen pagina zie je alle evenementen die Bslim organiseert.',
+                                },
+                                {
+                                   backgroundColor: '#8bc34a',
+                                   image: <Icon size={120} name={ 'account' } style={{ color: 'white' }} />,
+                                   title: 'Inloggen',
+                                   subtitle: 'Door op onze app in te loggen kun je stempels verzamelen bij elk Bslim evenement.',
+                                },
+                                {
+                                   backgroundColor: '#8bc34a',
+                                   image: <Icon size={120} name={ 'bell' } style={{ color: 'white' }} />,
+                                   title: 'Notificaties',
+                                   subtitle: 'Dankzij de notificaties blijf je altijd op de hoogte.',
+                                }
+                                ]}
+                            />
+                        )}
                     </ThemeContext.Provider>
-                }
+                )}
                 {this.state.loading &&
                     <View style={{justifyContent: 'center', alignItems: 'center', width: Dimensions.get('window').width, height: Dimensions.get('window').height, backgroundColor: '#94D600'}}>
                         <Image  style = {{width: 350, height: 225, marginTop: 100}}
