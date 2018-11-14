525db13c063c6f0690a9a761339cfe61c863d399
Bert de Boer <bertde.boer@hotmail.com>
2018-11-14 17:30:57 +0100

    registration fixed

--- app/screens/Registration.js
+++ app/screens/Registration.js
@@ -34,11 +34,11 @@ export default class Registration extends Component {
     // the user to type their password twice to avoid typing errors
     //the 'succesfull' state variable is used to display the snackbar when logged in
     this.state = {
-      email: "bb@bb.nl",
-      firstPassword: "123456",
-      secondPassword: "123456",
-      firstName: "Bërt",
-      lastName: "De Boër",
+      email: "",
+      firstPassword: "",
+      secondPassword: "",
+      firstName: "",
+      lastName: "",
       succesfull: false
     };
   }
